655bdb86200e27c26cb685b537351463217ac080
jirist <jiristanc@gmail.com>
2018-01-24 05:04:30 +0000

    calculates Pcts for each category

--- expenseCalculator.js
+++ expenseCalculator.js
@@ -109,9 +109,12 @@
                    *          variables declared in Task 4.  
                   *******************************************************************************************************/   
 
-
+                  totalExpenses = shelterAmt + foodAmt + householdAmt + transportationAmt + miscellaneousAmt;
                   shelterPct = 100 * (shelterAmt / totalExpenses);
                   foodPct = 100 * (foodAmt / totalExpenses);
+                  householdPct = 100 * (householdAmt / totalExpenses);
+                  transportationPct = 100 * (transportationAmt / totalExpenses);
+                  miscellaneousPct = 100 * (miscellaneousAmt / totalExpenses);
 
 
                   /******************************************************************************************************
